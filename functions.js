e963183edb0faa6e1afd097ac5d1f7291972c994
Shenil Dodhia <shenil.dodhia@yale.edu>
2013-02-27 22:28:09 -0500

    working on the enters in middle of doc

--- functions.js
+++ functions.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+$.fn.selectRange = function(start, end) {
+    return this.each(function() {
+        if (this.setSelectionRange) {
+            this.focus();
+            this.setSelectionRange(start, end);
+        } else if (this.createTextRange) {
+            var range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', start);
+            range.select();
+        }
+    });
+};
+
+function setCaretToPos (input, pos) {
+  setSelectionRange(input, pos, pos);
+}
+
+
+
 function updateBox(wikipediaPage, box, pos, curval)
 {
 	var req1 = $.ajax(
@@ -43,18 +64,22 @@ function updateBox(wikipediaPage, box, pos, curval)
 				//actval = actval.subString(actval.indexOf(curval) + curval.length() + 1);    
 
 		
-				box.val(curval + "\n" + text + newval);
+				box.val(curval.substring(0, pos) + "\n" + text + curval.substring(pos) + "\n");
 				box.scrollTop(9999).focus();
 
 				//alert(wikipediaPage + text);
 
-				box.trigger({type: 'keypress', which: 35});
-
+				//box.trigger({type: 'keypress', which: 35});
+				box.focus();
+				$(box).selectRange(pos+text.length+2,pos+text.length+2);	
+				//box.setSelectionRange(pos,pos);
+				//setCaretToPos(box,pos);
 
 			}
 			catch (err)
 			{
-				box.val(curval + "\n");
+				box.val(curval.substring(0,pos)  + "\n" + curval.substring(pos));
+				$(box).selectRange(pos+1 ,pos+1);
 			}
 		}
 
@@ -74,9 +99,13 @@ $('#input').on("keypress", function(e) {
 		var value   = $(this).val();
 		pos     = $(this).prop('selectionStart');        // cursor position
 		//$(this).val(value.substring(0,pos) + "HELLO" + value.substring(pos));
-		var lastline = value.lastIndexOf("\n");
-		$(this).val(value + "\n");
-		updateBox(value.substring(lastline+1,pos), $(this), pos, value);
+		var endLine = value.substring(0, pos).lastIndexOf("\n");
+		if (endLine == -1)
+			endLine = value.lastIndexOf("\n");
+//var lastline = value.lastIndexOf("\n");
+			$(this).val(value.substring(0, pos) + "\n" + value.substring(pos));
+		console.log(value.substring(endLine+1, pos));
+		updateBox(value.substring(endLine+1,pos), $(this), pos, value);
 		//$(this).scrollTop(1);
 		//  alert(value);
 		//alert (pos);
