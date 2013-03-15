51b871ed26927e321dca89dd3d43552e5608bc0b
earllee <earlvlee@gmail.com>
2013-03-14 22:49:53 -0400

    save text working.

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// Earl: This works with passport-dropbox. Authenticates properly. Have token,
-// token secret, and a user, but don't know where to move next.
-
 // Load dependencies
 var express = require('express');
 var routes = require('./routes');
@@ -8,8 +5,6 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var Dropbox = require('dropbox');
-var passport = require('passport');
-var DropboxStrategy = require('passport-dropbox').Strategy;
 var keys = require('./keys');
 
 // Create Express object
@@ -20,13 +15,10 @@ var DROPBOX_APP_KEY = keys.dropboxAppKey;
 var DROPBOX_APP_SECRET = keys.dropboxAppSecret;
 
 var client = new Dropbox.Client({
-  key: "Nlo4FSFkSkA=|QpwDRe2cRVnNap3sKxLywfO8pM245+xXmQuWH2g5lQ==", sandbox: true
-});
+  key: "Nlo4FSFkSkA=|QpwDRe2cRVnNap3sKxLywfO8pM245+xXmQuWH2g5lQ==", 
+  sandbox: true});
+
 client.authDriver(new Dropbox.Drivers.NodeServer(8191));
-//client.authDriver(new Dropbox.Drivers.Popup({
-//  receiverURL: 'http://127.0.0.1:3000/'}));
-//client.authDriver(new Dropbox.Drivers.Redirect());
-//client.authDriver(new Dropbox.Drivers.NodeServer(8191));
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -46,41 +38,42 @@ app.get('/', function(req, res){
   res.render('index', { user: req.user });
 });
 
+app.post('/', function(req, res) {
+  client.writeFile('testSave.txt', req.param('body'), function(error, stat) {
+    if (error) {
+      return showError(error);
+    }
+    console.log('Succesful save.');
+  });
+});
+
 app.get('/account', function(req, res){
-  res.render('account', { user: req.user });
+  client.getUserInfo(function(error, userInfo) {
+    res.render('account', { user: userInfo});
+  
+  });
 });
 
 app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-// GET /auth/dropbox
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Dropbox authentication will involve redirecting
-//   the user to dropbox.com.  After authorization, Dropbox will redirect the user
-//   back to this application at /auth/dropbox/callback
 app.get('/auth/dropbox', function(req, res) {
 
-client.authenticate(function(error, client) {
-  client.writeFile("hello_world.txt", "Hello, world!\n", function(error, stat) {
-    if (error) {
-        return showError(error);  // Something went wrong.
-        }
-      
-        alert("File saved as revision " + stat.revisionTag);
-      });
-  client.getUserInfo(function(error, userInfo) {
-        console.log("Hello, " + userInfo.name + "!");
-      });
+  client.authenticate(function(error, client) {
+    client.writeFile("hello_world.txt", "Hello, world!\n", 
+    function(error, stat) {
+      if (error) {
+        return showError(error); 
+      }
+      console.log("File saved as revision " + stat.revisionTag);
     });
+    client.getUserInfo(function(error, userInfo) {
+      console.log("Hello, " + userInfo.name + "!");
+    });
+    res.render('index');
   });
-
-// GET /auth/dropbox/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-
+});
 
 //app.get('/', routes.index);
 //app.get('/users', user.list);
