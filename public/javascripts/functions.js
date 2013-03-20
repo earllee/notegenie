6573428f232d44a865e3c149e516b7b576e08c76
earllee <earlvlee@gmail.com>
2013-03-19 23:46:50 -0400

    fixed enter handler for font size changer. preview text update

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -253,7 +253,7 @@ $(document).ready(function() {
 
   $('#markdownMode').on('click', function(e){
     if ($('#input').val() === '')  // Broken
-    $('#input').val('Using this tool\n ---------------\n \n This page lets you create HTML by entering text in a simple format that\'s easy to read and write.\n \n - Type Markdown text in the left window - See the HTML in the right \n Markdown is a lightweight markup language based on the formatting conventions that people naturally use in email.  As [John Gruber] writes on the [Markdown site] [1]:\n\n > The overriding design goal for Markdown\'s\n > formatting syntax is to make it as readable\n > as possible. The idea is that a\n > Markdown-formatted document should be\n > publishable as-is, as plain text, without\n > looking like it\'s been marked up with tags\n > or formatting instructions.\n \n This document is written in Markdown; you can see the plain-text version on the left.  To get a feel for Markdown\'s syntax, type some text into the left window and watch the results in the right.  You can see a Markdown syntax guide by switching the right-hand window from *Preview* to *Syntax Guide*.\n \n Showdown is a Javascript port of Markdown.  You can get the full [source code] by clicking on the version number at the bottom of the page.\n \n **Start with a [blank page] or edit this document in the left window.**\n \n [john gruber]: http://daringfireball.net/\n [1]: http://daringfireball.net/projects/markdown/\n [source code]: http://www.attacklab.net/showdown-v0.9.zip\n [blank page]: ?blank=1 "Clear all text"\n \n \n ## Syntax highlighting\n \n When combined with [highlight.js][] this starts looking as a kind of IDE :-)\n \n HTML:\n \n <h1>HTML code</h1>\n <p class="some">This is an example</p>\n \n Python:\n \n def func():\n for i in [1, 2, 3]:\n print "%s" % i\n \n \n [highlight.js]: http://softwaremaniacs.org/soft/highlight/en/\n');
+    $('#input').val('Using this tool\n ---------------\n \n This page lets you create HTML by entering text in a simple format that\'s easy to read and write.\n \n - Type Markdown text in the left window - See the HTML in the right \n Markdown is a lightweight markup language based on the formatting conventions that people naturally use in email.  As [John Gruber] writes on the [Markdown site] [1]:\n\n > The overriding design goal for Markdown\'s\n > formatting syntax is to make it as readable\n > as possible. The idea is that a\n > Markdown-formatted document should be\n > publishable as-is, as plain text, without\n > looking like it\'s been marked up with tags\n > or formatting instructions.\n \n This document is written in Markdown; you can see the plain-text version on the left.  To get a feel for Markdown\'s syntax, type some text into the left window and watch the results in the right.  You can see a Markdown syntax guide by switching the right-hand window from *Preview* to *Syntax Guide*.\n \n Showdown is a Javascript port of Markdown.  You can get the full [source code] by clicking on the version number at the bottom of the page.\n \n **Start with a [blank page] or edit this document in the left window.**\n \n [john gruber]: http://daringfireball.net/\n [1]: http://daringfireball.net/projects/markdown/\n [source code]: http://www.attacklab.net/showdown-v0.9.zip\n [blank page]: ?blank=1 "Clear all text"\n \n \n ## Syntax highlighting\n \n When combined with [highlight.js][] this starts looking as a kind of IDE :-)\n \n HTML:\n \n \t <h1>HTML code</h1>\n \t <p class="some">This is an example</p>\n \n Python:\n \n \t def func():\n \t for i in [1, 2, 3]:\n \t print "%s" % i\n \n \n [highlight.js]: http://softwaremaniacs.org/soft/highlight/en/\n');
     togglePreviewMode();
     e.preventDefault();
   });
@@ -266,15 +266,28 @@ $(document).ready(function() {
 
   //Change font size
   $('#font-size').on("change", function(e) {
+      changeFontSize(e);
+
+  });
+
+  var KEYCODE_ENTER = 13;
+  $('#font-size').on("keydown", function(e) {
+    if (e.keyCode == KEYCODE_ENTER) {
+      e.preventDefault();
+      changeFontSize(e);
+      return false;
+    }
+  });
+
+  function changeFontSize(event) {
     //$('.text').css("font-size", $(this).val() + "px");
-    var newSize = $(this).val();
+    var newSize = $('#font-size').val();
     var sizeChange = newSize - ngw.fontSize;
     $('#input, #preview, h6, h5, h4, h3, h2, h1').css("font-size", '+=' + sizeChange);
     ngw.fontSize = newSize;
     localStorage.setItem('fontSize', newSize);
-    e.preventDefault();
-    return false;
-  });
+  }
+
 
   //Logo Tooltip
   $('.brand').tooltip({trigger: 'hover', delay: {hide: 1000}, placement: 'top'});
