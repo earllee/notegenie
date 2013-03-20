cc419dcaf1ad17dbc8ba7adf65f8829180e77950
earllee <earlvlee@gmail.com>
2013-03-20 10:11:06 -0400

    enabled gzip compressiong.

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
+  app.use(express.compress());
   app.use(express.logger('dev'));
   app.use(express.cookieParser());
   app.use(express.bodyParser());
